1ca85334e40951c6dca2c11d6d9fc273a44a5ab2
yossefezzat <45318576+yossefezzat@users.noreply.github.com>
2020-03-23 03:11:17 +0200

    Trigram-Model

--- model.py
+++ model.py
@@ -1,76 +1,55 @@
-import nltk as nltk
-from collections import Counter, defaultdict
 import os as os
+import nltk as nltk
+from collections import defaultdict
 
+def read_Folder_data(folder_name):
+    folder_files = os.listdir(folder_name)
+    all_files = "" 
+    for file in folder_files:
+        file = open("International news//" + file , "r" , encoding="utf8").read()
+        all_files += file +"\n"
+    return all_files
 
-# Create a placeholder for model
-model = defaultdict(lambda: defaultdict(lambda: 0))
-'''
-folder_files = os.listdir("International news")
-all_files = "" 
-
-for file in folder_files:
-    file = open("International news//" + file , "r" , encoding="utf8").read()
-    all_files += file +"\n"
-   
-f = open('data_international.txt' , 'w' ,  encoding="utf8")
-f.write(all_files)
-f.close()
-
-'''
-
-file = open("international_news.txt" , "r" , encoding="utf8")
-data = file.read()
-file.close()
-data_tokenized = nltk.word_tokenize(data)
-
-trigram_data = nltk.trigrams(data_tokenized)
-#print(list(trigram_data)[-1:])
-
-
-for words in list(trigram_data):
-    w1, w2, w3 = words
-    model[(w1,w2)][w3]+=1
-
-count = 0 
-for w1_w2 in model:
-    total_count = float(sum(model[w1_w2].values()))
-    for w3 in model[w1_w2]:
-        model[w1_w2][w3] /= total_count 
-           
-           
-
-        
     
+def read_file_data(file):
+    file = open( file , "r" , encoding="utf8")
+    data = file.read()
+    file.close()
+    return data
 
-#print(model[tuple("غربية" ,"دول")].keys())
-    
-  
-    
+def tokenize_words(data):
+    data_tokenized = nltk.word_tokenize(data)
+    return data_tokenized
+
+def make_trigram(data_tokenized):
+    trigram_list = []
+    for i in range(len(data_tokenized)-2) :
+        trigram_list.append((data_tokenized[i] ,data_tokenized[i+1], data_tokenized[i+2]))
+    return trigram_list
+
+def build_model(data_tokenized):
+    trigram_data = make_trigram(data_tokenized)
+    model = defaultdict(lambda: defaultdict(lambda: 0))
+    for words in list(trigram_data):
+        w1, w2, w3 = words
+        model[(w1,w2)][w3]+=1
+
+    total_count = 0 
+    for w1_w2 in model:
+        total_count = float(sum(model[w1_w2].values()))
+        for w3 in model[w1_w2]:
+            model[w1_w2][w3] /= total_count 
+    return model
+
+
+data = read_file_data("international_news.txt")
+tokens = tokenize_words(data)
+model = build_model(tokens)
+
 text = "يحذر في الوقت ذاته من الاستخدام المفرط للقوة"
 text = text.split()
 
-print(model[tuple(text[-2:])].keys())
-
-'''
-sentence_finished = False
- 
-while not sentence_finished:
-  # select a random probability threshold  
-  r = 0.6
-  accumulator = .0
-  for word in model[tuple(text[-2:])].keys():
-      accumulator += model[tuple(text[-2:])][word]
-      # select words that are above the probability threshold
-      if accumulator >= r:
-          text.append(word)
-          break
-
-  if text[-2:] == [None, None]:
-      sentence_finished = True
- 
-print (' '.join([t for t in text if t]))
-'''
+print(model[tuple(text[-2:])].keys() , model[tuple(text[-2:])].values())
 
     
     
@@ -85,18 +64,3 @@ print (' '.join([t for t in text if t]))
     
     
     
-    
-    
-    
-
-'''
-String_trigram = trigrams(file.split())
-
-for grams in String_trigram:
-    print (grams)
-
-
-
-for gram in String_trigram:
-    print(nltk.probability.FreqDist())
-'''
