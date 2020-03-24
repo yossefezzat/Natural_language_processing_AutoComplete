f38e025a01ca6e11d406ed94671437473978d549
yossefezzat <45318576+yossefezzat@users.noreply.github.com>
2020-03-24 13:54:28 +0200

    clean coding

--- model.py
+++ model.py
@@ -9,7 +9,6 @@ def read_Folder_data(folder_name):
         file = open("International news//" + file , "r" , encoding="utf8").read()
         all_files += file +"\n"
     return all_files
-
     
 def read_file_data(file):
     file = open( file , "r" , encoding="utf8")
@@ -33,34 +32,39 @@ def build_model(data_tokenized):
     for words in list(trigram_data):
         w1, w2, w3 = words
         model[(w1,w2)][w3]+=1
-
+        
     total_count = 0 
-    for w1_w2 in model:
-        total_count = float(sum(model[w1_w2].values()))
-        for w3 in model[w1_w2]:
-            model[w1_w2][w3] /= total_count 
+    for st_nd_word in model:
+        total_count = float(sum(model[st_nd_word].values()))
+        for rd_word in model[st_nd_word]:
+            model[st_nd_word][rd_word] /= total_count 
     return model
 
 
-data = read_file_data("international_news.txt")
-tokens = tokenize_words(data)
-model = build_model(tokens)
+def run_model(data_file):
+    data = read_file_data(data_file)
+    tokens = tokenize_words(data)
+    model = build_model(tokens)
+    return model
 
-text = "يحذر في الوقت ذاته من الاستخدام المفرط للقوة"
+
+def sorting_result(model, text):
+    model = sorted(model[tuple(text[-2:])].items(), key = lambda kv:(kv[1], kv[0]),reverse=True)
+    return model
+
+
+def get_top_10(model_sorted_result):
+    print(len(model_sorted_result) )
+    output_size = 10 if len(model_sorted_result)>10  else len(model_sorted_result)
+    for i in range(output_size):
+        print(model_sorted_result[i][0] , "    " , round(model_sorted_result[i][1],6))
+    if output_size == 0: 
+        print("لا توجد اقتراحات")
+
+model = run_model("international_news.txt")
+text =  input("Enter your Input : ")
 text = text.split()
+model = sorting_result(model , text)
+get_top_10(model)
 
-print(model[tuple(text[-2:])].keys() , model[tuple(text[-2:])].values())
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+#" يحذر في الوقت ذاته من الاستخدام المفرط للقوة في العراق وقال"
